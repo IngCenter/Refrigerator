54b6a2d1c943caf8b978a42c473f486699dd0e53
DimaPopkov <55281177+DimaPopkov@users.noreply.github.com>
2020-11-06 20:08:51 +0400

    оплоуд

--- Холодильник/Main.Designer.cs
+++ Холодильник/Main.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.deadProducts = new System.Windows.Forms.Button();
             this.countButton = new System.Windows.Forms.Button();
             this.contents = new System.Windows.Forms.Button();
             this.addProductsButton = new System.Windows.Forms.Button();
@@ -37,25 +36,12 @@
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // deadProducts
-            // 
-            this.deadProducts.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.deadProducts.Location = new System.Drawing.Point(352, 13);
-            this.deadProducts.Margin = new System.Windows.Forms.Padding(4);
-            this.deadProducts.Name = "deadProducts";
-            this.deadProducts.Size = new System.Drawing.Size(190, 91);
-            this.deadProducts.TabIndex = 0;
-            this.deadProducts.Text = "Просрочка";
-            this.deadProducts.UseVisualStyleBackColor = true;
-            this.deadProducts.Click += new System.EventHandler(this.DeadProducts_Click);
-            // 
             // countButton
             // 
             this.countButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.countButton.Location = new System.Drawing.Point(352, 112);
-            this.countButton.Margin = new System.Windows.Forms.Padding(4);
+            this.countButton.Location = new System.Drawing.Point(264, 91);
             this.countButton.Name = "countButton";
-            this.countButton.Size = new System.Drawing.Size(190, 91);
+            this.countButton.Size = new System.Drawing.Size(142, 74);
             this.countButton.TabIndex = 1;
             this.countButton.Text = "Количество продуктов";
             this.countButton.UseVisualStyleBackColor = true;
@@ -64,10 +50,9 @@
             // contents
             // 
             this.contents.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.contents.Location = new System.Drawing.Point(352, 211);
-            this.contents.Margin = new System.Windows.Forms.Padding(4);
+            this.contents.Location = new System.Drawing.Point(264, 171);
             this.contents.Name = "contents";
-            this.contents.Size = new System.Drawing.Size(190, 91);
+            this.contents.Size = new System.Drawing.Size(142, 74);
             this.contents.TabIndex = 2;
             this.contents.Text = "Содержимое";
             this.contents.UseVisualStyleBackColor = true;
@@ -76,10 +61,9 @@
             // addProductsButton
             // 
             this.addProductsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.addProductsButton.Location = new System.Drawing.Point(13, 13);
-            this.addProductsButton.Margin = new System.Windows.Forms.Padding(4);
+            this.addProductsButton.Location = new System.Drawing.Point(10, 11);
             this.addProductsButton.Name = "addProductsButton";
-            this.addProductsButton.Size = new System.Drawing.Size(175, 140);
+            this.addProductsButton.Size = new System.Drawing.Size(131, 114);
             this.addProductsButton.TabIndex = 4;
             this.addProductsButton.Text = "Добавить продукты";
             this.addProductsButton.UseVisualStyleBackColor = true;
@@ -88,10 +72,9 @@
             // cookButton
             // 
             this.cookButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.cookButton.Location = new System.Drawing.Point(13, 161);
-            this.cookButton.Margin = new System.Windows.Forms.Padding(4);
+            this.cookButton.Location = new System.Drawing.Point(10, 131);
             this.cookButton.Name = "cookButton";
-            this.cookButton.Size = new System.Drawing.Size(175, 141);
+            this.cookButton.Size = new System.Drawing.Size(131, 115);
             this.cookButton.TabIndex = 5;
             this.cookButton.Text = "Приготовить что-то";
             this.cookButton.UseVisualStyleBackColor = true;
@@ -100,24 +83,23 @@
             // fridgeButton
             // 
             this.fridgeButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.fridgeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.fridgeButton.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.fridgeButton.Location = new System.Drawing.Point(196, 13);
-            this.fridgeButton.Margin = new System.Windows.Forms.Padding(4);
+            this.fridgeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.fridgeButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.fridgeButton.Location = new System.Drawing.Point(147, 11);
             this.fridgeButton.Name = "fridgeButton";
-            this.fridgeButton.Size = new System.Drawing.Size(148, 221);
+            this.fridgeButton.Size = new System.Drawing.Size(111, 234);
             this.fridgeButton.TabIndex = 3;
+            this.fridgeButton.Text = "Техническое состояние и обслуживание";
             this.fridgeButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.fridgeButton.UseVisualStyleBackColor = true;
             this.fridgeButton.Click += new System.EventHandler(this.Fridge_Click);
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(196, 242);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(264, 11);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(148, 60);
+            this.button1.Size = new System.Drawing.Size(141, 74);
             this.button1.TabIndex = 7;
             this.button1.TabStop = false;
             this.button1.Text = "Коментарии";
@@ -126,19 +108,17 @@
             // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(554, 317);
+            this.ClientSize = new System.Drawing.Size(417, 264);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.cookButton);
             this.Controls.Add(this.addProductsButton);
             this.Controls.Add(this.fridgeButton);
             this.Controls.Add(this.contents);
             this.Controls.Add(this.countButton);
-            this.Controls.Add(this.deadProducts);
-            this.Margin = new System.Windows.Forms.Padding(4);
-            this.MaximumSize = new System.Drawing.Size(572, 364);
-            this.MinimumSize = new System.Drawing.Size(572, 364);
+            this.MaximumSize = new System.Drawing.Size(433, 303);
+            this.MinimumSize = new System.Drawing.Size(433, 303);
             this.Name = "Main";
             this.Text = "Панель управления";
             this.ResumeLayout(false);
@@ -146,8 +126,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button deadProducts;
         private System.Windows.Forms.Button countButton;
         private System.Windows.Forms.Button contents;
         private System.Windows.Forms.Button fridgeButton;

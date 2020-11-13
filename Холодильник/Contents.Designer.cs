80b708412bb21ad3737d6989900cfef728a0fcdb
Daniel <daniel.gevorgyan25@gmail.com>
2020-11-13 19:31:38 +0400

    нормальное сохранение

--- Холодильник/Contents.Designer.cs
+++ Холодильник/Contents.Designer.cs
@@ -28,21 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ProductName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProductBirthdate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProductLifetime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProductCount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ProductName,
+            this.ProductBirthdate,
+            this.ProductLifetime,
+            this.ProductCount});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1025, 416);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // ProductName
+            // 
+            this.ProductName.HeaderText = "Название";
+            this.ProductName.MinimumWidth = 6;
+            this.ProductName.Name = "ProductName";
+            this.ProductName.ReadOnly = true;
+            this.ProductName.Width = 320;
+            // 
+            // ProductBirthdate
+            // 
+            this.ProductBirthdate.HeaderText = "Дата упаковки";
+            this.ProductBirthdate.MinimumWidth = 6;
+            this.ProductBirthdate.Name = "ProductBirthdate";
+            this.ProductBirthdate.ReadOnly = true;
+            this.ProductBirthdate.Width = 192;
+            // 
+            // ProductLifetime
+            // 
+            this.ProductLifetime.HeaderText = "Срок годности";
+            this.ProductLifetime.MinimumWidth = 6;
+            this.ProductLifetime.Name = "ProductLifetime";
+            this.ProductLifetime.Width = 192;
+            // 
+            // ProductCount
+            // 
+            this.ProductCount.HeaderText = "Количество";
+            this.ProductCount.MinimumWidth = 6;
+            this.ProductCount.Name = "ProductCount";
+            this.ProductCount.Width = 192;
             // 
             // Contents
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(1025, 416);
+            this.Controls.Add(this.dataGridView1);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Contents";
             this.Text = "Содержимое";
             this.Load += new System.EventHandler(this.Contents_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProductName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProductBirthdate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProductLifetime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProductCount;
     }
 }
--- Холодильник/Contents.cs
+++ Холодильник/Contents.cs
@@ -21,9 +21,9 @@ namespace Fridgerator
 
         private void Contents_Load(object sender, EventArgs e)
         {
-            List<string> products = Program.Select("SELECT * FROM Products");
+            List<string> products = Program.Select("SELECT Name, DateBegin, LifeTime, Unit, Count FROM Products");
 
-            int y = 30;
+            /*int y = 30;
 
             Label nameLabel = new Label { Text = "Имя", Location = new Point(labelsX[0], y) };
             Label dateBeginLabel = new Label { Text = "Когда куплены", Location = new Point(labelsX[1], y) };
@@ -31,11 +31,11 @@ namespace Fridgerator
             Label countLabel = new Label { Text = "Количество", Location = new Point(labelsX[3], y) };
 
             Controls.AddRange(new Label[4] { nameLabel, dateBeginLabel, lifeTimeLabel, countLabel});
-
+            
             for (int i = 0; i < products.Count; i += 6)
             {
                 y += 30;
-                Color backColor = DateTime.Parse(products[i + 1]).AddDays(int.Parse(products[i + 2])) > DateTime.Today ? Color.FromArgb(192, 128, 128) : Color.Transparent;
+                Color backColor = DateTime.Parse(products[i + 1]).AddDays(int.Parse(products[i + 2])) > DateTime.Today ? Color.Transparent : Color.FromArgb(192, 128, 128);
 
                 for (int j = 0; j < 4; j ++)
                 {
@@ -50,6 +50,24 @@ namespace Fridgerator
                     Label label = new Label { Text = products[i + j], Location = new Point(labelsX[j], y), BackColor = backColor };
                     Controls.Add(label);
                 }
+            }*/
+
+            for (int i = 0; i < products.Count; i += 5)
+            {
+                string[] row = new string[4];
+
+                Color backColor = DateTime.Parse(products[i + 1]).AddDays(int.Parse(products[i + 2])) > DateTime.Today.AddDays(2) ? Color.White : Color.FromArgb(192, 192, 128);
+
+                if (DateTime.Parse(products[i + 1]).AddDays(int.Parse(products[i + 2])) < DateTime.Today)
+                    backColor = Color.FromArgb(192, 128, 128);
+
+                row[0] = products[i];
+                row[1] = products[i + 1].Remove(products[i + 1].Length - 8);
+                row[2] = products[i + 2] + " дней";
+                row[3] = products[i + 4] + " " + products[i + 3];
+
+                dataGridView1.Rows.Add(row);
+                dataGridView1.Rows[i/5].DefaultCellStyle.BackColor = backColor;
             }
         }
     }

6b16e35265ac393601b7d73a4de3ed3d4d18eba4
Daniel <daniel.gevorgyan25@gmail.com>
2020-12-04 20:50:11 +0400

    изменения

--- Холодильник/Recipe.cs
+++ Холодильник/Recipe.cs
@@ -24,13 +24,44 @@ namespace Fridgerator
 
         private void CookButton_Click(object sender, EventArgs e)
         {
-            Program.Insert($"INSERT INTO `RecipesHistory` (`Recipe`, `Portions`, `CookDate`) VALUES (`{comboRecipe.Text}`, {1}, STR_TO_DATE({DateTime.Today.ToShortDateString()}, %d.%m.%y))");
+            List<string> products = Program.Select("SELECT `Type`, `Count`, `Unit` FROM Products WHERE DATEDIFF(CURDATE(), DateBegin) < LifeTime");
+
+            if(products[0] == "12345678765432")
+                Program.Insert($"INSERT INTO `RecipesHistory` (`Recipe`, `Portions`, `CookDate`) VALUES (`{comboRecipe.Text}`, {1}, STR_TO_DATE({DateTime.Today.ToShortDateString()}, %d.%m.%y))");
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void AddRecipeButton_Click(object sender, EventArgs e)
         {
             AddRecipe ar = new AddRecipe();
             ar.Show();
+        }
+
+        private void ComboRecipe_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateDataGridView();
+
+            List<string> products = Program.Select("SELECT `Type`, `Count`, `Unit` FROM Products ");// + 
+                                                                                                    //"WHERE DATEDIFF(CURDATE(), DateBegin) < LifeTime");
+
+            cookButton.Enabled = false;
+        }
+
+        private void UpdateDataGridView()
+        {
+            dataGridView1.Rows.Clear();
+
+            List<string> contents = Program.Select($"SELECT `Product`, `Count`, `Unit` FROM RecipesContents WHERE RecipeName = '{comboRecipe.Text}'");
+
+            for (int i = 0; i < contents.Count; i += 3)
+            {
+                string[] row = new string[3];
+
+                row[0] = contents[i];
+                row[1] = contents[i + 1];
+                row[2] = contents[i + 2];
+
+                dataGridView1.Rows.Add(row);
+            }
         }
     }
 }

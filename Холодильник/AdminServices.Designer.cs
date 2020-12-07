8147f3d29b7a7b27cc9b027577752637f96e57dc
DimaPopkov <55281177+DimaPopkov@users.noreply.github.com>
2020-12-07 20:57:31 +0400

    JOINны и подобие фильтрации
    и оформление

--- Холодильник/AdminServices.Designer.cs
+++ Холодильник/AdminServices.Designer.cs
@@ -33,6 +33,7 @@
             this.CityCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.TroubleCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.PriseCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,12 +45,14 @@
             this.CityCol,
             this.TroubleCol,
             this.PriseCol});
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(9, 10);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(776, 426);
+            this.dataGridView1.Size = new System.Drawing.Size(582, 276);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // FridgeModelCol
             // 
@@ -79,12 +82,25 @@
             this.PriseCol.Name = "PriseCol";
             this.PriseCol.Width = 125;
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(9, 291);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(582, 63);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Фильтр использованых продуктов за неделю";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // AdminServices
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "AdminServices";
             this.Text = "AdminServices";
             this.Load += new System.EventHandler(this.AdminServices_Load);
@@ -100,5 +116,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn CityCol;
         private System.Windows.Forms.DataGridViewTextBoxColumn TroubleCol;
         private System.Windows.Forms.DataGridViewTextBoxColumn PriseCol;
+        private System.Windows.Forms.Button button1;
     }
 }

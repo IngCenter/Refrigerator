e78e094e495e93d401a5e4437ca6d388b04d2cf6
mabramovsd <beavisabra@yandex.ru>
2020-12-20 15:00:35 +0400

    Творчество Димы

--- Холодильник/Admin.Designer.cs
+++ Холодильник/Admin.Designer.cs
@@ -36,6 +36,9 @@
             this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
             this.label1 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,12 +56,12 @@
             // 
             // Name
             // 
-            this.Name.HeaderText = "Назание";
+            this.Name.HeaderText = "Название";
             this.Name.Name = "Name";
             // 
             // Call
             // 
-            this.Call.HeaderText = "Колличество";
+            this.Call.HeaderText = "Количество";
             this.Call.Name = "Call";
             // 
             // button1
@@ -78,7 +81,7 @@
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
             this.dateTimePicker1.TabIndex = 2;
-            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            this.dateTimePicker1.Value = new System.DateTime(2020, 11, 14, 20, 40, 0, 0);
             // 
             // dateTimePicker2
             // 
@@ -86,7 +89,7 @@
             this.dateTimePicker2.Name = "dateTimePicker2";
             this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
             this.dateTimePicker2.TabIndex = 3;
-            this.dateTimePicker2.Value = new System.DateTime(2020, 11, 11, 19, 58, 0, 0);
+            this.dateTimePicker2.Value = new System.DateTime(2020, 12, 14, 19, 58, 0, 0);
             this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
             // 
             // label1
@@ -101,17 +104,47 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(328, 36);
+            this.comboBox1.Location = new System.Drawing.Point(329, 56);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 5;
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(326, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Название";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(69, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "от";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(69, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "до";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // Admin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dateTimePicker2);
@@ -120,9 +153,9 @@
             this.Controls.Add(this.dataGridView1);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Text = "Что мы ели";
-            this.Load += new System.EventHandler(this.Admin_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -130,11 +163,14 @@
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Call;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.DateTimePicker dateTimePicker2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Call;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
--- Холодильник/Admin.cs
+++ Холодильник/Admin.cs
@@ -15,12 +15,17 @@ namespace Fridgerator
         public Admin()
         {
             InitializeComponent();
+
+            List<string> fillbox =
+                Program.Select("SELECT DISTINCT product FROM RecipesContents");
+            comboBox1.DataSource = fillbox;
+
         }
 
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-           
+     
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -29,47 +34,39 @@ namespace Fridgerator
 
             string dateFrom = dateTimePicker1.Value.ToString("yyyy-MM-dd");
             string dateFrom2 = dateTimePicker2.Value.ToString("yyyy-MM-dd");
-            string command =           "SELECT RecipesContents.Product," +
-                                       " SUM(Portions * RecipesContents.Count)" +
+            string command = "SELECT RecipesContents.Product," +
+                                       " SUM(RecipesHistory.Count * RecipesContents.Count)" +
                                        " FROM RecipesHistory" +
                                        " JOIN RecipesContents ON RecipesContents.RecipeName = RecipesHistory.Recipe" +
-                                       " WHERE CookDate < '" + dateFrom + "' AND CookDate > '" + dateFrom2 + "'";
+                                       " WHERE CookDate < '" + dateFrom2 + "' AND CookDate > '" + dateFrom + "'";
 
             if (comboBox1.Text != "")
             {
-                ///
+                command = command + " AND RecipesContents.Product = '" + comboBox1.Text + "'";
             }
-            
 
-            command += " GROUP BY RecipesContents.Product";
-            List<string> x1 = Program.Select(command);
+                command += " GROUP BY RecipesContents.Product";
+                List<string> x1 = Program.Select(command);
 
-            for (int i = 0; i < x1.Count; i += 2)
-            {
-                string[] row = new string[2];
-                row[0] = x1[i];
-                row[1] = x1[i + 1];
+                for (int i = 0; i < x1.Count; i += 2)
+                {
+                    string[] row = new string[2];
+                    row[0] = x1[i];
+                    row[1] = x1[i + 1];
 
-                dataGridView1.Rows.Add(row);
-            }
+                    dataGridView1.Rows.Add(row);
+                }
+          
+
+
         }
 
-        private void Admin_Load(object sender, EventArgs e)
+        private void label4_Click(object sender, EventArgs e)
         {
 
         }
 
         private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
